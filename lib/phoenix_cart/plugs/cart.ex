b61550160e46831ea5b59ef4c0fbc49a0b7443c2
Cory Schmitt <cory@schmitty.me>
2015-07-21 16:35:22 -0700

    Alias Order Model in Cart Plug

--- lib/phoenix_cart/plugs/cart.ex
+++ lib/phoenix_cart/plugs/cart.ex
@@ -1,15 +1,16 @@
 defmodule PhoenixCart.Plugs.Cart do
   import Plug.Conn
   alias PhoenixCart.Repo
+  alias PhoenixCart.Order
 
   def init(default), do: default
 
   def call(conn, _default) do
     if get_session(conn, :cart) do
-      cart = Repo.get(PhoenixCart.Order, get_session(conn, :cart))
+      cart = Repo.get(Order, get_session(conn, :cart))
       assign(conn, :order, cart)
     else
-      cart = Repo.insert!(%PhoenixCart.Order{status: "cart"})
+      cart = Repo.insert!(%Order{status: "cart"})
       conn = put_session(conn, :cart, cart.id)
       assign(conn, :order, cart)
     end
